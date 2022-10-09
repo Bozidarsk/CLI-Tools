c76a34dee40314103e05ddd85d863a90b304bf7c
Bozidarsk <bozidarkabahcijski@gmail.com>
2022-10-09 17:58:12 +0300

    Bug fixes.

--- file/main.cs
+++ file/main.cs
@@ -3,51 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
-public class Attributes 
-{
-    private List<FileAttributes> A = new List<FileAttributes>();
-    private List<string> Str = new List<string>();
-
-    public int Count 
-    {
-    	get { return Math.Min(A.Count, Str.Count); }
-    }
-
-	public string Get(FileAttributes _A) 
-    {
-    	return Str[A.IndexOf(_A)];
-    }
-
-    public FileAttributes Get(string _str) 
-    {
-    	return A[Str.IndexOf(_str)];
-    }
-
-    public Attributes() 
-    {
-        List<FileAttributes> _A = new List<FileAttributes>();
-    	_A.Add(FileAttributes.Hidden);
-    	_A.Add(FileAttributes.ReadOnly);
-    	_A.Add(FileAttributes.Normal);
-    	_A.Add(FileAttributes.Temporary);
-    	_A.Add(FileAttributes.Temporary);
-    	_A.Add(FileAttributes.Temporary);
-    	_A.Add(FileAttributes.System);
-
-    	List<string> _Str = new List<string>();
-    	_Str.Add("hidden");
-    	_Str.Add("readonly");
-    	_Str.Add("normal");
-    	_Str.Add("tmp");
-    	_Str.Add("temp");
-    	_Str.Add("temporary");
-    	_Str.Add("system");
-
-    	A = _A;
-    	Str = _Str;
-    }
-}
-
 class main 
 {
 	static string GetStringAt(string main, int startPos, int endPos) 
@@ -69,72 +24,14 @@ class main
         return output;
     }
 
-    static string GetToParentFolder(string dir) 
+    static int Max(int a, int b) { return (a > b) ? a : b; }
+
+    static FileAttributes GetAttributes(string input) 
     {
-    	if (dir.IndexOf("\\") <= -1) { return dir; }
-
-    	if (dir.LastIndexOf(".") < dir.LastIndexOf("\\")) 
-    	{	
-	    	while (true) 
-	    	{
-	    		dir = dir.Remove(dir.Length - 1, 1);
-	    		if (dir.Length <= 0) { break; }
-	    		if (System.Convert.ToString(dir[dir.Length - 1]) == "\\") { break; }
-	    	}
-
-	    	return dir.Remove(dir.Length - 1, 1);
-    	}
-
-    	if (dir.LastIndexOf(".") > dir.LastIndexOf("\\")) 
-    	{	
-	    	bool startRemoving = false;
-	    	int i = dir.Length - 1;
-
-	    	while (i >= 0) 
-	    	{
-	    		if (startRemoving) { dir = dir.Remove(dir.Length - 1, 1); }
-	    		if (dir.Length <= 0) { break; }
-	    		if (System.Convert.ToString(dir[dir.Length - 1]) == "\\" && startRemoving) { break; }
-	    		i--;
-
-	    		if (System.Convert.ToString(dir[i]) == "\\") { startRemoving = true; }
-	    	}
-
-	    	return dir.Remove(dir.Length - 1, 1);
-    	}
-
-    	return dir;
-    }
-
-    static void MoveFile(string src, string dist) 
-    {
-    	if (src == "" || dist == "" || src == dist) { return; }
-    	string content = File.ReadAllText(src);
-		File.Delete(src);
-
-		string fullDist = dist + "\\" + src;
-    	if (dist == "." || dist == ".\\") { src = ""; dist = Directory.GetCurrentDirectory(); fullDist = dist; }
-
-		if (fullDist.Contains("\\\\\\")) { fullDist = fullDist.Replace("\\\\\\", "\\"); }
-		if (fullDist.Contains("\\\\")) { fullDist = fullDist.Replace("\\\\", "\\"); }
-		Console.WriteLine(content);
-
-		var file = File.Create(fullDist); file.Close();
-		File.WriteAllText(fullDist, content);
-    }
-
-    static void CopyFile(string src, string dist) 
-    {
-    	if (src == "" || dist == "") { return; }
-    	string content = File.ReadAllText(src);
-    	if (src == dist) { src += " (copy)"; }
-
-		string fullDist = dist + "\\" + src;
-
-		if (fullDist.Contains("\\\\\\")) { fullDist = fullDist.Replace("\\\\\\", "\\"); }
-		if (fullDist.Contains("\\\\")) { fullDist = fullDist.Replace("\\\\", "\\"); }
-
-		File.WriteAllText(fullDist, content);
+    	FileAttributes output = 0;
+    	string[] tokens = input.ToLower().Replace(" ", "").Split('|');
+    	for (int i = 0; i < tokens.Length; i++) { output |= attributes[tokens[i]]; }
+    	return output;
     }
 
 	static void Main(string[] args) 
@@ -143,50 +40,58 @@ class main
 		if (args[0] == "help" || args[0] == "--help" || args[0] == "-h" || args[0] == "/h" || args[0] == "/?" || args[0] == "-?") 
 		{
 			Console.WriteLine(
-				"Usage:\n\tfile \"filename\" [option] [arg]\n\nOptions:\n" + 
-				"\t-wt \"text\"\tOverrides the file with text.\n" + 
-				"\t-at \"text\"\tAppends the file with text.\n" + 
-				"\t-a \"attribute\"\tSets attribute to the file. (hidden | readonly | normal | system | (tmp | temp | temporary))\n" + 
-				"\t-mv \"directory\"\tMoves the file to directory.\n" + 
-				"\t-cp \"directory\"\tCopies the file to directory.\n" + 
-				"\t-rm\tRemoves the file.\n" + 
-				"\t-mkdir \"directory\"\tMakes a new directory.\n" + 
-				"\t-rmdir \"directory\"\tRemoves the directory.\n"
+				"Usage:\n\tfile \"filename\" [option] [arg]\n" + 
+				"\nOptions:\n" + 
+				"\t-wt, --write-text text              \tOverrides the file with text.\n" + 
+				"\t-at, --append-text text             \tAppends the file with text.\n" + 
+				"\t-a, --attributes attribute          \tSets attribute to the file. (hidden | readonly | normal | system | tmp)\n" + 
+				"\t-mv, --move directory               \tMoves the file to directory.\n" + 
+				"\t-cp, --copy directory               \tCopies the file to directory.\n" + 
+				"\t-rm, --remove file                  \tRemoves the file.\n" + 
+				"\t-mkdir, --make-directory directory  \tMakes a new directory.\n" + 
+				"\t-rmdir, --remove-directory directory\tRemoves the directory.\n"
 			);
+			return;
 		}
 
-		int wt = Array.IndexOf(args, "-wt");
-		int at = Array.IndexOf(args, "-at");
-		int a = Array.IndexOf(args, "-a");
+		int wt = Max(Array.IndexOf(args, "-wt"), Array.IndexOf(args, "--write-text"));
+		int at = Max(Array.IndexOf(args, "-at"), Array.IndexOf(args, "--append-text"));
+		int a = Max(Array.IndexOf(args, "-a"), Array.IndexOf(args, "--attributes"));
 
-		int mv = Array.IndexOf(args, "-mv");
-		int cp = Array.IndexOf(args, "-cp");
+		int mv = Max(Array.IndexOf(args, "-mv"), Array.IndexOf(args, "--move"));
+		int cp = Max(Array.IndexOf(args, "-cp"), Array.IndexOf(args, "--copy"));
 
-		int rm = Array.IndexOf(args, "-rm");
-		int rmdir = Array.IndexOf(args, "-rmdir");
-		int mkdir = Array.IndexOf(args, "-mkdir");
+		int rm = Max(Array.IndexOf(args, "-rm"), Array.IndexOf(args, "--remove"));
+		int rmdir = Max(Array.IndexOf(args, "-rmdir"), Array.IndexOf(args, "--remove-directory"));
+		int mkdir = Max(Array.IndexOf(args, "-mkdir"), Array.IndexOf(args, "--make-directory"));
 
-		if (args.Length % 2 == 0 && rm == -1 && mkdir == -1) { return; }
-		if (args[0].IndexOf(".") <= 0 && rm == -1 && mkdir == -1) { return; }
+		bool create = (~wt + ~at + ~a + ~mv + ~cp + ~rm + ~rmdir + ~mkdir) == 0;
 
-    	if (wt >= 0 && at >= 0) { return; }
-    	if (mv >= 0 && cp >= 0) { return; }
-
-		Attributes attributes = new Attributes();
 		string extention = GetStringAt(args[0], args[0].IndexOf("."), args[0].Length - 1);
-		string newContent = "";
+		string content = "";
 
-		try { newContent = File.ReadAllText("C:\\Tools\\Source\\default" + extention.Remove(0, 1)); }
+		try { content = File.ReadAllText("C:\\Tools\\Source\\default" + extention); }
 		catch {}
 
-		if (wt == -1 && at == -1) { File.WriteAllText(args[0], newContent); }
+		if (create) { File.WriteAllText(args[0], content); }
 		if (wt >= 0) { File.WriteAllText(args[0], args[wt + 1]); }
 		if (at >= 0) { File.AppendAllText(args[0], args[at + 1]); }
-		if (a >= 0) { File.SetAttributes(args[0], attributes.Get(args[a + 1].ToLower())); }
-		if (mv >= 0) { MoveFile(args[0], args[mv + 1]); }
-		if (cp >= 0) { CopyFile(args[0], args[cp + 1]); }
+		if (a >= 0) { File.SetAttributes(args[0], GetAttributes(args[a + 1])); }
+		if (mv >= 0) { File.Move(args[0], args[mv + 1]); }
+		if (cp >= 0) { File.Copy(args[0], args[cp + 1]); }
 		if (rm >= 0) { File.Delete(args[rm + 1]); }
 		if (rmdir >= 0) { Directory.Delete(args[rmdir + 1]); }
 		if (mkdir >= 0) { Directory.CreateDirectory(args[mkdir + 1]); }
 	}
+
+	static Dictionary<string, FileAttributes> attributes = new Dictionary<string, FileAttributes>() 
+	{
+    	{ "hidden", FileAttributes.Hidden },
+    	{ "readonly", FileAttributes.ReadOnly },
+    	{ "normal", FileAttributes.Normal },
+    	{ "tmp", FileAttributes.Temporary },
+    	{ "temp", FileAttributes.Temporary },
+    	{ "temporary", FileAttributes.Temporary },
+    	{ "system", FileAttributes.System }
+	};
 }
